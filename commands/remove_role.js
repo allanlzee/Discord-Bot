ccfc3e9083b3082f51346f42b97e7b02e88665a5
Allan Zhou <allanlinzhou@gmail.com>
2021-05-08 20:39:28 -0400

    Added Remove Role in master

--- commands/remove_role.js
+++ commands/remove_role.js
@@ -3,11 +3,38 @@ module.exports = {
     description: "removes a role from a user",
     
     execute(message, args) {
+        let oldMember = message.mentions.users.first();
+        let newMember = message.guild.member(oldMember); 
+        let newRole = args[1].toLowerCase(); 
 
-        if (message.member.roles.cache.has("840319423410733147")) {
-            // BETA bot
-            message.member.roles.remove("840319423410733147"); 
-            message.channel.send("Role removed."); 
+        const admin = "840332334753382481";
+        const programmer = "840318933910683690"; 
+        const functionalBot = "840319357627924590"; 
+        const BETAbot = "840319423410733147"; 
+        const closedBot = "840319468835176457";
+        
+        if (message.member.roles.cache.has(admin)) {
+            switch(newRole) {
+                case 'programmer':
+                    newMember.roles.remove(programmer).catch(console.error);
+                    message.channel.send(`Removed role Programmer to ${newMember}`); 
+                    break; 
+                case 'functional-bot':
+                    newMember.roles.remove(functionalBot).catch(console.error); 
+                    message.channel.send(`Removed role Functional Bot to ${newMember}`); 
+                    break; 
+                case 'beta-bot':
+                    newMember.roles.remove(BETAbot).catch(console.error);
+                    message.channel.send(`Removed role BETA Bot to ${newMember}`); 
+                    break;
+                case 'closed-bot':
+                    newMember.roles.remove(closedBot).catch(console.error);
+                    message.channel.send(`Removed role Closed Bot to ${newMember}`); 
+                    break; 
+                default:
+                    message.channel.send(`Error: Cannot remove role ${newRole} to ${newMember}`); 
+                    break; 
+            }
         }
-    }
+    } 
 }
